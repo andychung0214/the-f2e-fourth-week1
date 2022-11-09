bff1ae1bfded1e2d8407cf989ec410dc1266eab4
Andy Chung <andy6756@gmail.com>
2022-11-10 00:16:07 +0800

    準備 Lottie 套件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'the-f2e-fourth-week1';
+  loadingOpts: any;
+  constructor() {
+
+// this.loadingOpts: AnimationOptions = {
+//   path: '/assets/json/loading.json',
+//   autoplay: true,
+//   loop: true,
+//   initialSegment: [10, 88]
+// };
+// options: AnimationOptions = {
+//   path: '/assets/json/loading.json',
+// };
+// animationCreated(animationItem: AnimationItem): void {
+//   console.log(animationItem);
+// }
+  }
+
+  ngOnInit(): void {}
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+// import { LottieModule } from 'ngx-lottie';
+// import player from 'lottie-web';
 
+// Note we need a separate function as it's required
+// by the AOT compiler.
+// export function playerFactory() {
+//   return player;
+// }
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,8 +21,9 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    // LottieModule.forRoot({ player: playerFactory })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
