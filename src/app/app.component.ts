53507b41bc88c9dd16ac23436518387a2ed3b6d2
AndyChung <andy6756@gmail.com>
2022-11-10 17:45:13 +0800

    新增修改相關樣式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'gsap';
+import {TimelineMax} from 'gsap/gsap-core';
+import Draggable from 'gsap/Draggable';
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
+// gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'the-f2e-fourth-week1';
   loadingOpts: any;
+  // scrollBox =  new TimelineMax({paused: true, reversed: true,
+  //   scrollTrigger: {
+  //     trigger: '.box',
+  //     pin: true,
+  //     start: 'top top',
+  //     end: 'bottom bottom',
+  //     markers: true,
+  //     toggleActions: 'play none none reverse',
+  //   }
+  //   });
   constructor() {
 
 // this.loadingOpts: AnimationOptions = {
@@ -25,8 +40,15 @@ export class AppComponent {
 // }
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.scrollTgr();
+    // gsap.registerPlugin(ScrollTrigger, Draggable, MotionPathPlugin);
+  }
 
-
+  // scrollTgr() {
+  //   this.scrollBox.to('.box', { y: 30, opacity: 0 });
+  //   this.scrollBox.to('.box', { y: 0, opacity: 1 });
+  //   this.scrollBox.play();
+  // }
 
 }
